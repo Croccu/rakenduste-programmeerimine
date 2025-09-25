ced116547f6a3230059bc8c517980d679dfcc53d
Croccu <144142847+Croccu@users.noreply.github.com>
2025-09-25 13:44:18 +0300

    feat: backend cats controller, routes and middlewares

--- lessons/2025-09-25/rico/backend/index.js
+++ lessons/2025-09-25/rico/backend/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const catsRoutes = require("./routes/cats.routes");
+
+app.use(express.json());
+
+app.use("/cats", catsRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
