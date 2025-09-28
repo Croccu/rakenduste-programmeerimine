67b4a518f0f31066bba5704ef0a465574d3539c0
Croccu <144142847+Croccu@users.noreply.github.com>
2025-09-28 22:35:02 +0300

    feat: Add cors + Cat, SubmitCat component. Adjust App to use new components

--- lessons/2025-09-25/rico/backend/index.js
+++ lessons/2025-09-25/rico/backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 const catsRoutes = require("./routes/cats.routes");
-
-app.use(express.json());
 
 app.use("/cats", catsRoutes);
 
