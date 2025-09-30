cca106a118726607b7d12ac029aea31e2a3d4545
Croccu <144142847+Croccu@users.noreply.github.com>
2025-09-30 18:44:25 +0300

    duplicated cats project as base for todo

--- lessons/2025-09-25/todo/frontend/src/examples/PropDrilling.tsx
+++ lessons/2025-09-25/todo/frontend/src/examples/PropDrilling.tsx
@@ -1,22 +1,22 @@
-// import React from "react";
-// import { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
-// const PropDrilling = () => {
-//   const [weather, setWeather] = useState(":sun_with_face:");
+const PropDrilling = () => {
+  const [weather, setWeather] = useState(":sun_with_face:");
 
-//   return <Europe weather={weather} />;
-// };
+  return <Europe weather={weather} />;
+};
 
-// const Europe = ({ weather }) => {
-//   return <Estonia weather={weather} />;
-// };
+const Europe = ({ weather }) => {
+  return <Estonia weather={weather} />;
+};
 
-// const Estonia = ({ weather }) => {
-//   return <Tallinn weather={weather} />;
-// };
+const Estonia = ({ weather }) => {
+  return <Tallinn weather={weather} />;
+};
 
-// const Tallinn = ({ weather }) => {
-//   return <div>{weather}</div>;
-// };
+const Tallinn = ({ weather }) => {
+  return <div>{weather}</div>;
+};
 
-// export default PropDrilling;
+export default PropDrilling;
